9f2ec34b7ee8948e8328dc7fa8adcc598d8195d0
Darko-Martinovic <darko.martinovic@outlook.com>
2017-08-18 09:45:47 +0200

    Update test script

--- SqlClrCustomSendMail-master/SqlClrCustomSendMail/Test/Test.sql
+++ SqlClrCustomSendMail-master/SqlClrCustomSendMail/Test/Test.sql
@@ -1,5 +1,25 @@
-﻿
-EXEC [EMAIL].[CLRSendMail]	@profileName = N'SimpleTalk'
-						,@mailTo = N'darko.martinovic@outlook.hr'
-						,@mailSubject = N'Slanje putem outlooka'
-						,@mailBody = N'Outlook';
+﻿--Test sending e-mail
+EXEC [EMAIL].[CLRSendMail] @profileName = N'SimpleTalk'
+						  ,@mailTo = N'darko.martinovic@outlook.com'
+						  ,@mailSubject = N'Slanje putem outlooka'
+						  ,@mailBody = N'Outlook';
+--Test query to html
+DECLARE @body as nvarchar(max)
+SET @body = EMAIL.QueryToHtml('SELECT * FROM EMAIL.PROFILES', '', 'EMAIL.Profiles', '#', 2, 0, 'ST_BLUE')
+EXEC [EMAIL].[CLRSendMail] @profileName = N'SimpleTalk'
+						  ,@mailTo = N'darko.martinovic@outlook.com'
+						  ,@mailSubject = N'Test QueryToHtml'
+						  ,@mailBody = @body;
+--Test ConCat Html
+SET @body = (SELECT
+		EMAIL.ConCatHtml(@body, (SELECT
+				EMAIL.QueryToHtml('SELECT
+ *
+FROM EMAIL.Configurations','',
+				'EMAIL.Configuration', '#', 2, 0, 'ST_RED'))
+		));
+
+EXEC [EMAIL].[CLRSendMail] @profileName = N'SimpleTalk'
+						  ,@mailTo = N'darko.martinovic@outlook.com'
+						  ,@mailSubject = N'Test QueryToHtml'
+						  ,@mailBody = @body;
